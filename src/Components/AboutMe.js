8ea8b5e29271657cb0e06aee20d2e48dcb863043
VSKSurya <suryasree22@gmail.com>
2021-11-25 10:16:39 +0530

    change has done

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -14,7 +14,7 @@ const AboutMe = () => {
 
         <div className="row">
           <div className="col-lg-4">
-            <img src="/dist/img/profile-img.jpg" className="img-fluid" alt="" />
+            <img src="../dist/img/profile-img.jpg" className="img-fluid" alt="" />
           </div>
           <div className="col-lg-8 pt-4 pt-lg-0 content">
             <h3>UI/UX Designer &amp; Web Developer.</h3>
