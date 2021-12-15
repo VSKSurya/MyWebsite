fce810b5364e8fdd6d008df518a445dd8a3f9427
kalidas.vsk <kalidas.vsk@schoolnetindia.com>
2021-12-15 13:55:48 +0530

    Update Graphs

--- src/Components/Chart.js
+++ src/Components/Chart.js
@@ -3,15 +3,9 @@ import { Line } from '@ant-design/charts';
 
 const Page: React.FC = () => {
   const data = [
-    { year: '1991', value: 3 },
-    { year: '1992', value: 4 },
-    { year: '1993', value: 3.5 },
-    { year: '1994', value: 5 },
-    { year: '1995', value: 4.9 },
-    { year: '1996', value: 6 },
-    { year: '1997', value: 7 },
-    { year: '1998', value: 9 },
-    { year: '1999', value: 13 },
+    { year: '2000', value: 3 },
+    { year: '2004-2008 : TCS', value: 4.5 },
+    { year: '2008 - 2021 : Schoolnet India Limited', value: 13.5 }
   ];
 
   const config = {
